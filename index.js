057df52b0824616836848fb8a891d130010e49dd
TuGG <tugg.solo@gmail.com>
2015-11-19 23:06:08 +0700

    change port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 4000));
 
 app.use('/node_modules', express.static(__dirname + '/node_modules'));
 app.use('/css', express.static(__dirname + '/css'));
