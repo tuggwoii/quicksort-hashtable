c44b903001225f204baa0bd770f1674824f68f5b
TuGG <tugg.solo@gmail.com>
2015-11-27 00:16:06 +0700

    done

--- js/app-controller.js
+++ js/app-controller.js
@@ -2,6 +2,7 @@ app.controller('AppController', function ($scope) {
 	
 	$scope.outputs = [];
 	$scope.array = [];
+	$scope.hashArray = [];
 	$scope.inputs = '';
 	$scope.tabs = 0;
 	
@@ -35,7 +36,7 @@ app.controller('AppController', function ($scope) {
 	function randomInput (inputs) {
 		var inputs = [];
 		for(var i = 0; i < 100; i++) {
-			var randomNumber = Math.floor((Math.random() * 999) + 1);
+			var randomNumber = Math.floor((Math.random() * 1000) + 1);
 			inputs.push(randomNumber);
 		}
 		return inputs.join(' ');
@@ -45,6 +46,24 @@ app.controller('AppController', function ($scope) {
 		for(var i = 0; i < A.length; i++) {
 			A[i] = parseInt(A[i]);
 		}
+	}
+
+	function createHashArray () {
+	    $scope.hashArray = [];
+	    for (var i = 0; i < 50; i++) {
+	        $scope.hashArray.push({ storage: [] });
+	    }
+	}
+
+	function hashTableByDivisionMethod (A) {
+	    for (var i = 0; i < A.length; i++) {
+	        A[i] = parseInt(A[i]);
+	        if (!isNaN(A[i])) {
+	            var index = A[i] % 50;
+	            $scope.hashArray[index].storage.push(A[i]);
+	        }
+	    }
+	    console.log($scope.hashArray);
 	}
 	
 	$scope.appStart = function () {
@@ -58,10 +77,16 @@ app.controller('AppController', function ($scope) {
 	}
 	
 	$scope.submit = function () {
+        //QUICK SORT
 		$scope.array = $scope.inputs.split(' ');
 		parseIntInput($scope.array);
 		quickSort($scope.array, 0, $scope.array.length - 1);
 		$scope.outputString = $scope.array.join(' ');
+
+	    //HASH TABLE
+		createHashArray();
+		var inputArray = $scope.inputs.split(' ');
+		hashTableByDivisionMethod(inputArray);
 	};
 	
 	$scope.random = function () {
